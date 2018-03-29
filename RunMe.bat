8cc44ab21ca5e4a1ba398c6cc287cdf441b2f4bf
raaappana <raappanaa@comcast.net>
2018-03-28 20:41:04 -0700

    Add files via upload

---
+++ RunMe.bat
@@ -0,0 +1,40 @@
+@echo off
+set BatDir="%~dp0\batFiles"
+set origDir="%~dp0"
+
+echo Executing RunMe.bat
+echo.
+
+echo *===================================================*
+echo * DO NOT USE SPECIAL CHARACTERS OR SPACES IN INPUTS *
+echo *===================================================*
+echo.
+
+FOR /F "tokens=* delims=" %%x IN (WatchedFolder.txt) DO SET WatchedFolderLocationSans=%%x
+set WatchedFolderLocation="%WatchedFolderLocationSans%"
+
+rem Look at what our WatchedFolder is
+set Watched
+
+echo.
+echo ------------------------------------------------------------------------------------------------------------------------
+echo.
+echo.
+
+:programStart
+
+CALL GetSampleSetNameAndInitSmears.bat
+
+set "restartU="
+
+echo.
+set /p restartU="Restart Program? (y/n || default: no):"
+echo.
+
+if %restartU% == y (goto programStart)
+if %restartU% == yes (goto programStart)
+
+echo.
+echo Closing Program.
+
+rem PING localhost -n 6 >NUL
